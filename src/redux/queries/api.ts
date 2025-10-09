e8644e949190365cdb09f72a4ad55a3031232cb0
Hussain <hkosaimi@gmail.com>
2025-10-09 18:19:22 +0300

    new

--- src/redux/queries/api.ts
+++ src/redux/queries/api.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:4001",
+  // baseUrl: "http://localhost:4001",
   // baseUrl: "https://backend.webschema.online",
+  baseUrl: "https://auknotes-backend.webschema.online",
   credentials: "include",
 });
 
